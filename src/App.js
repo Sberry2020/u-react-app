d37883617034676db5dd6c4a6301d06cd939730a
Sberry2020 <69343800+Sberry2020@users.noreply.github.com>
2021-01-11 23:45:57 -0700

    Added GitHub and Netlify repo Url

--- src/App.js
+++ src/App.js
@@ -5,7 +5,16 @@ export default function App() {
   return (
     <div className="App">
       Weather App
-    
+      <footer>
+        This project was coded by Aksana Kurbanova and is {""}
+        <a href="https://github.com/Sberry2020/u-react-app" target="_blank" >
+          open-sourced on GitHub {""}
+        </a>
+        and also posted on {""}
+        <a href="https://agitated-lovelace-6dab26.netlify.app" target="_blank">
+Netlify.com
+        </a>
+        </footer>
     </div>
   );
 }
